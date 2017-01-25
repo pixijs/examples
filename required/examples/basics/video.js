07226393a965315a91a5908beceb1340f126a38c
Matt Karl <matt.karl@jibo.com>
2017-01-25 14:46:58 -0500

    Add play button to video example

--- required/examples/basics/video.js
+++ required/examples/basics/video.js
@@ -1,13 +1,45 @@
 var app = new PIXI.Application(800, 600, { transparent: true });
 document.body.appendChild(app.view);
 
-// create a video texture from a path
-var texture = PIXI.Texture.fromVideo('required/assets/testVideo.mp4');
+// Create play button that can be used to trigger the video
+var button = new PIXI.Graphics()
+    .beginFill(0x0, 0.5)
+    .drawRoundedRect(0, 0, 100, 100, 10)
+    .endFill()
+    .beginFill(0xffffff)
+    .moveTo(36, 30)
+    .lineTo(36, 70)
+    .lineTo(70, 50);
 
-// create a new Sprite using the video texture (yes it's that easy)
-var videoSprite = new PIXI.Sprite(texture);
+// Position the button
+button.x = (app.renderer.width - button.width) / 2;
+button.y = (app.renderer.height - button.height) / 2;
 
-videoSprite.width = app.renderer.width;
-videoSprite.height = app.renderer.height;
+// Enable interactivity on the button
+button.interactive = true;
+button.buttonMode = true;
 
-app.stage.addChild(videoSprite);
+// Add to the stage
+app.stage.addChild(button);
+
+// Listen for a click/tap event to start playing the video
+// this is useful for some mobile platforms
+button.on('pointertap', onPlayVideo);
+
+function onPlayVideo() {
+
+    // Don't need the button anymore
+    button.destroy();
+
+    // create a video texture from a path
+    var texture = PIXI.Texture.fromVideo('required/assets/testVideo.mp4');
+
+    // create a new Sprite using the video texture (yes it's that easy)
+    var videoSprite = new PIXI.Sprite(texture);
+
+    // Stetch the fullscreen
+    videoSprite.width = app.renderer.width;
+    videoSprite.height = app.renderer.height;
+
+    app.stage.addChild(videoSprite); 
+}

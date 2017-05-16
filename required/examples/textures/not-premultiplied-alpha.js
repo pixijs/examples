c64d44ee6bf26c675999265bb7f4a6c27af2cb60
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-05-16 21:06:28 +0300

    ultimate test for png/dds premultiplied

--- required/examples/textures/not-premultiplied-alpha.js
+++ required/examples/textures/not-premultiplied-alpha.js
@@ -1,4 +1,4 @@
-var app = new PIXI.Application(800, 600, {backgroundColor: 0x707070});
+var app = new PIXI.Application(800, 600, {backgroundColor: 0x308030});
 document.body.appendChild(app.view);
 
 //compressed textures setup
@@ -7,8 +7,9 @@ PIXI.compressedTextures.detectExtensions(app.renderer);
 var loader = new PIXI.loaders.Loader();
 loader.pre(PIXI.compressedTextures.imageParser());
 
-// in this example you can see what can happen when pixi does not respect premultiplied alpha
-// actually, DDS is never premultiplied in videomemory, but pixi doesn't know about it
+// for PNG in videomemory, premultiplied=true, for DDS its false
+// what if we upload them, but change the flag after it?
+// sprites 2 and 3 are wrong
 
 var textStyle = { fill: 0xffffff };
 function createSprite(texture, text) {
@@ -19,31 +20,33 @@ function createSprite(texture, text) {
 	return sprite;
 }
 
-loader.add('house_png', 'required/assets/compressed/dracula_house_4.png')
-	.add('house_png_2', 'required/assets/compressed/dracula_house_4.png')
-    .add('house_dds', 'required/assets/compressed/dracula_house_4.dds')
-	.add('house_dds_2', 'required/assets/compressed/dracula_house_4.dds')
+loader.add('house_png', 'required/assets/compressed/shop_svyaznoy.png')
+	.add('house_png_2', 'required/assets/compressed/shop_svyaznoy.png?v=2')
+    .add('house_dds', 'required/assets/compressed/shop_svyaznoy.dds')
+	.add('house_dds_2', 'required/assets/compressed/shop_svyaznoy.dds?v=2')
     .load(function(loader, resources) {
-        var spr1 = createSprite(resources.house_png.texture, "PNG premultiplied=true");
-        var spr2 = createSprite(resources.house_png_2.texture, "PNG premultiplied=false");
-		var spr3 = createSprite(resources.house_dds.texture, "DDS premultiplied=true");
-		var spr4 = createSprite(resources.house_dds_2.texture, "DDS premultiplied=false");
+        var spr1 = createSprite(resources.house_png.texture, "PNG premultiplied=true, good");
+        var spr2 = createSprite(resources.house_png_2.texture, "PNG premultiplied=false, bad");
+		var spr3 = createSprite(resources.house_dds.texture, "DDS premultiplied=true, bad");
+		var spr4 = createSprite(resources.house_dds_2.texture, "DDS premultiplied=false, good");
 		
+		resources.house_png.texture.baseTexture.premultipliedAlpha = true;
+		resources.house_dds.texture.baseTexture.premultipliedAlpha = true;
 		resources.house_png_2.texture.baseTexture.premultipliedAlpha = false;
 		resources.house_dds_2.texture.baseTexture.premultipliedAlpha = false;
 		
 		
 		spr1.position.set(250, 250);
-		spr1.scale.set(0.5);
+		spr1.scale.set(0.8);
 		
-		spr2.position.set(450, 250);
-		spr2.scale.set(0.5);
+		spr2.position.set(550, 250);
+		spr2.scale.set(0.8);
 		
 		spr3.position.set(250, 500);
-		spr3.scale.set(0.5);
+		spr3.scale.set(0.8);
 		
-		spr4.position.set(450, 500);
-		spr4.scale.set(0.5);
+		spr4.position.set(550, 500);
+		spr4.scale.set(0.8);
 		
         app.stage.addChild(spr1, spr2, spr3, spr4);
     });

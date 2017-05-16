9f808bca15e1c6cbc85c46e7587ba85684aa0ea3
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-05-16 21:16:36 +0300

    remove copyrighted content, sorry )

--- required/examples/textures/not-premultiplied-alpha.js
+++ required/examples/textures/not-premultiplied-alpha.js
@@ -17,13 +17,15 @@ function createSprite(texture, text) {
 	sprite.addChild(new PIXI.Text(text, textStyle));
 	sprite.anchor.set(0.5, 1);
 	sprite.children[0].anchor.set(0.5, 0);
+	sprite.interactive=true;
+	sprite.on('click', function() { this.alpha = 1.7 - this.alpha; } );
 	return sprite;
 }
 
-loader.add('house_png', 'required/assets/compressed/shop_svyaznoy.png')
-	.add('house_png_2', 'required/assets/compressed/shop_svyaznoy.png?v=2')
-    .add('house_dds', 'required/assets/compressed/shop_svyaznoy.dds')
-	.add('house_dds_2', 'required/assets/compressed/shop_svyaznoy.dds?v=2')
+loader.add('house_png', 'required/assets/compressed/dracula_house_4.png')
+	.add('house_png_2', 'required/assets/compressed/dracula_house_4.png?v=2')
+    .add('house_dds', 'required/assets/compressed/dracula_house_4.dds')
+	.add('house_dds_2', 'required/assets/compressed/dracula_house_4.dds?v=2')
     .load(function(loader, resources) {
         var spr1 = createSprite(resources.house_png.texture, "PNG premultiplied=true, good");
         var spr2 = createSprite(resources.house_png_2.texture, "PNG premultiplied=false, bad");
@@ -37,16 +39,17 @@ loader.add('house_png', 'required/assets/compressed/shop_svyaznoy.png')
 		
 		
 		spr1.position.set(250, 250);
-		spr1.scale.set(0.8);
+		spr1.scale.set(0.6);
 		
 		spr2.position.set(550, 250);
-		spr2.scale.set(0.8);
+		spr2.scale.set(0.6);
 		
-		spr3.position.set(250, 500);
-		spr3.scale.set(0.8);
+		spr3.position.set(250, 550);
+		spr3.scale.set(0.6);
 		
-		spr4.position.set(550, 500);
-		spr4.scale.set(0.8);
-		
+		spr4.position.set(550, 550);
+		spr4.scale.set(0.6);
+	
         app.stage.addChild(spr1, spr2, spr3, spr4);
+		
     });

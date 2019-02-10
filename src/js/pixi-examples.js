7e9a4408a2052ca4f8d0387f18e4d5b040101dde
Dave Moore <themoonrat@gmail.com>
2019-02-10 17:39:59 +0000

    Able to set 'versions' for test. Going to an invalid example will not show the canvas nor the source code

--- src/js/pixi-examples.js
+++ src/js/pixi-examples.js
@@ -5,31 +5,57 @@ function getParameterByName(name) {
     return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 
+function getMajorPixiVersion(pixiVersionString) {
+    var majorVersion = 5;
+
+    if (pixiVersionString.substr(0, 1) === 'v') {
+        majorVersion = parseInt(pixiVersionString.substr(1, 1), 10);
+    } else {
+        if (pixiVersionString === 'release') {
+            majorVersion = 4;
+        }
+    }
+
+    return majorVersion
+}
+
 jQuery(document).ready(function($) {
     window.onpopstate = function(event) {
-        bpc.version = getParameterByName('v') || 'release';
+        bpc.pixiVersionString = getParameterByName('v') || 'release';
         bpc.generateIFrameContent();
 
         $('.select-group .select li.selected').removeClass('selected');
-        var $selected = $('.select-group .select li[data-val="' + bpc.version + '"]');
+        var $selected = $('.select-group .select li[data-val="' + bpc.pixiVersionString + '"]');
         $selected.addClass('selected');
         $('.select-group .select .current').text($selected.text());
 
-        $('.main-content').animate({ scrollTop: 0 }, 300);
+        $('.main-content').animate({ scrollTop: 0 }, 200);
     };
 
     var bpc = bpc || {};
-    bpc.aniIn = 0.2;
-    bpc.aniOut = 0.3;
-    bpc.currentSource = '';
-    bpc.currentFilename = '';
-    bpc.version = getParameterByName('v') || 'release';
-    bpc.plugins = '';
+
+    bpc.pixiVersionString = getParameterByName('v') || 'release';
+    bpc.majorPixiVersion = getMajorPixiVersion(bpc.pixiVersionString);
+
+    bpc.exampleUrl = '';
+    bpc.exampleFilename = '';
+    bpc.exampleSourceCode = '';
+    bpc.exampleRequiredPlugins = [];
+    bpc.exampleAllowedVersions = [];
+
+    bpc.editorOptions = {
+        mode: 'javascript',
+        lineNumbers: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        viewportMargin: Infinity,
+        lineWrapping: true
+    };
 
     bpc.clickType = Modernizr.touchevents ? 'tap' : 'click';
+    bpc.animTime = 0.15;
 
-    bpc.resize = function() {
-    };
+    bpc.resize = function() {};
 
     // async script loading
     bpc.scriptsToLoad = ['https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js'];
@@ -49,13 +75,17 @@ jQuery(document).ready(function($) {
                 success: bpc.fileLoaded
             });
         }
-        if (bpc.scriptsToLoad.length === 0) bpc.loadComplete();
+
+        if (bpc.scriptsToLoad.length === 0) {
+            bpc.loadComplete();
+        }
     };
 
     bpc.fileLoaded = function() {
         bpc.scriptsLoaded++;
-        if (bpc.scriptsLoaded === bpc.scriptsToLoad.length)
+        if (bpc.scriptsLoaded === bpc.scriptsToLoad.length) {
             bpc.loadComplete();
+        }
     };
 
     bpc.loadComplete = function() {
@@ -67,7 +97,8 @@ jQuery(document).ready(function($) {
 
                 for (var j = 0; j < items.length; j++) {
                     var plugins = typeof items[j].plugins !== 'undefined' ? items[j].plugins.join(',') : '';
-                    html += '<li data-src="' + items[j].entry + '" data-plugins="' + plugins + '">' + items[j].title + '</li>';
+                    var versions = typeof items[j].versions !== 'undefined' ? items[j].versions.join(',') : '';
+                    html += '<li data-src="' + items[j].entry + '" data-plugins="' + plugins  + '" data-versions="' + versions + '">' + items[j].title + '</li>';
                 }
                 html += '</ul>';
 
@@ -102,51 +133,20 @@ jQuery(document).ready(function($) {
                     $('.select-group .select ul').append('<li data-val="' + data[i] + '">' + data[i] + '</li>');
                 }
 
-                var $selected = $('.select-group .select li[data-val="' + bpc.version + '"]');
+                var $selected = $('.select-group .select li[data-val="' + bpc.pixiVersionString + '"]');
                 $selected.addClass('selected');
-                $('.select-group .select .current').text($selected.text());
+                $('.select-group .select .current').text($selected.text())
             });
 
         });
     };
 
-    bpc.openMobileNav = function() {
-        TweenMax.to('#line1', 0.2, { y: 0, ease: Linear.easeNone });
-        TweenMax.to('#line2', 0, { alpha: 0, ease: Linear.easeNone, delay: 0.2 });
-        TweenMax.to('#line3', 0.2, { y: 0, ease: Linear.easeNone });
-
-        TweenMax.to('#line1', 0.2, { rotation: 45, ease: Quart.easeOut, delay: 0.2 });
-        TweenMax.to('#line3', 0.2, { rotation: -45, ease: Quart.easeOut, delay: 0.2 });
-
-        $('.main-nav').addClass('mobile-open');
-    };
-
-    bpc.closeMobileNav = function() {
-        TweenMax.to('#line1', 0.2, { rotation: 0, ease: Linear.easeNone, delay: 0 });
-        TweenMax.to('#line3', 0.2, { rotation: 0, ease: Linear.easeNone, delay: 0 });
-
-        TweenMax.to('#line2', 0, { alpha: 1, ease: Quart.easeOut, delay: 0.2 });
-        TweenMax.to('#line1', 0.2, { y: -8, ease: Quart.easeOut, delay: 0.2 });
-        TweenMax.to('#line3', 0.2, { y: 8, ease: Quart.easeOut, delay: 0.2 });
-
-        $('.main-nav').removeClass('mobile-open');
-    };
-
-    /*bpc.drawLines = function(elem) {
-        var divHeight = parseInt(elem.height());
-        var lineHeight = parseInt(elem.css('line-height'));
-        var lines = divHeight / lineHeight;
-        $('.main-content .code-editor .line-numbers').html('');
-        for (var i = 1; i <= lines; i++) {
-            $('.main-content .code-editor .line-numbers').append(i+'<br/>');
-        }
-    };*/
-
     bpc.initNav = function() {
         $('.main-menu .section').on(bpc.clickType, function() {
-            $(this).next('ul').slideToggle(350);
+            $(this).next('ul').slideToggle(250);
             $(this).toggleClass('open');
         });
+
         $('.main-menu li').on(bpc.clickType, function() {
             if (!$(this).hasClass('selected')) {
                 $('.main-menu li.selected').removeClass('selected');
@@ -164,45 +164,112 @@ jQuery(document).ready(function($) {
                 ga('set', { page: page, title: title });
                 ga('send', 'pageview');
 
-                bpc.currentSource = 'examples/js/' + $(this).parent().attr('data-section') + '/' + $(this).attr('data-src');
-                bpc.currentFilename = $(this).attr('data-src');
-                bpc.plugins = $(this).attr('data-plugins');
+                bpc.exampleUrl = 'examples/js/' + $(this).parent().attr('data-section') + '/' + $(this).attr('data-src');
+                bpc.exampleFilename = $(this).attr('data-src');
+
+                var plugins = $(this).attr('data-plugins');
+                bpc.exampleRequiredPlugins = plugins === '' ? [] : plugins.split(',');
+
+                var versions = $(this).attr('data-versions');
+                bpc.exampleAllowedVersions = versions === '' ? [3, 4, 5] : versions.split(',').map(function(v) {return parseInt(v, 10)});
 
                 $.ajax({
                     url: 'examples/js/' + $(this).parent().attr('data-section') + '/' + $(this).attr('data-src'),
                     dataType: "text",
                     success: function(data) {
-                        if (bpc.plugins) {
-                            $('#code-header').text("Example Code (plugins used: " + bpc.plugins.replace(/,/g, ', ') + ")");
-                        } else {
-                            $('#code-header').text("Example Code");
-                        }
-
-                        bpc.jsSource = data;
-                        var html = data;
-                        $('.CodeMirror').remove();
-                        $('.main-content #code').html(html);
-
-                        var editorOptions = {
-                            mode: 'javascript',
-                            lineNumbers: true,
-                            styleActiveLine: true,
-                            matchBrackets: true,
-                            viewportMargin: Infinity,
-                            lineWrapping: true
-                        };
-
-                        bpc.editor = CodeMirror.fromTextArea(document.getElementById('code'), editorOptions);
-
-                        //bpc.drawLines($('.main-content .code-editor #code'));
+                        bpc.exampleSourceCode = data;
 
                         bpc.generateIFrameContent();
                     }
                 });
-
-                //$('#preview').attr('src', 'iframe.html?src='+bpc.currentSource+'&v='+bpc.version+'&plugins='+bpc.plugins);
             }
         });
+
+        bpc.generateIFrameContent = function() {
+            // Remove all iFrames and content
+            var iframes = document.querySelectorAll('iframe');
+            for (var i = 0; i < iframes.length; i++) {
+                iframes[i].parentNode.removeChild(iframes[i]);
+            }
+            $('#example').html('<iframe id="preview" src="blank.html"></iframe>');
+
+            // Generate HTML and insert into iFrame
+            var exampleJS = bpc.exampleSourceCode;
+            var pixiUrl = '';
+
+            if (bpc.pixiVersionString === 'local') {
+                pixiUrl = "dist/pixi.js"
+            } else if (bpc.majorPixiVersion === 3) { // pull v3 from github cdn
+                pixiUrl = 'https://cdn.rawgit.com/GoodBoyDigital/pixi.js/' + bpc.pixiVersionString + '/bin/pixi.js';
+            } else { // other versions come from S3
+                pixiUrl = 'https://d157l7jdn8e5sf.cloudfront.net/' + bpc.pixiVersionString + '/pixi.js';
+            }
+
+            var html = '<!DOCTYPE html><html><head><style>body,html{margin:0px;height:100%;overflow:hidden;}canvas{width:100%;height:100%;}</style></head><body>';
+            html += '<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>';
+            html += '<script src="' + pixiUrl + '"></script>';
+
+            if (bpc.majorPixiVersion < 5) {
+                html += '<script src="pixi-plugins/pixi-legacy.js"></script>';
+            }
+
+            for (i = 0; i < bpc.exampleRequiredPlugins.length; i++) {
+                html += '<script src="pixi-plugins/' + bpc.exampleRequiredPlugins[i] + '.js"></script>';
+            }
+
+            if (bpc.exampleAllowedVersions.indexOf(bpc.majorPixiVersion) > -1) {
+                html += '<script>window.onload = function(){' + exampleJS + '}</script></body></html>';
+            }
+
+            $('.CodeMirror').remove();
+            $('.main-content #code').html(bpc.exampleSourceCode);
+
+            if (bpc.exampleAllowedVersions.indexOf(bpc.majorPixiVersion) > -1) {
+                bpc.editor = CodeMirror.fromTextArea(document.getElementById('code'), bpc.editorOptions);
+
+                if (bpc.exampleRequiredPlugins.length) {
+                    $('#code-header').text("Example Code (plugins used: " + bpc.exampleRequiredPlugins.toString() + ")");
+                } else {
+                    $('#code-header').text("Example Code");
+                }
+
+                $('.example-frame').show();
+            } else {
+                var versionText = bpc.exampleAllowedVersions.length === 1 ? "version" : "versions";
+                $('#code-header').text("Example only works on PixiJS " + versionText + ": " + bpc.exampleAllowedVersions.toString());
+                $('.example-frame').hide();
+            }
+
+            var iframe = document.getElementById('preview');
+            var frameDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+            frameDoc.open();
+            frameDoc.write(html);
+            frameDoc.close();
+        };
+
+        bpc.openMobileNav = function() {
+            TweenMax.to('#line1', bpc.animTime, { y: 0, ease: Linear.easeNone });
+            TweenMax.to('#line2', 0, { alpha: 0, ease: Linear.easeNone, delay: bpc.animTime });
+            TweenMax.to('#line3', bpc.animTime, { y: 0, ease: Linear.easeNone });
+
+            TweenMax.to('#line1', bpc.animTime, { rotation: 45, ease: Quart.easeOut, delay: bpc.animTime });
+            TweenMax.to('#line3', bpc.animTime, { rotation: -45, ease: Quart.easeOut, delay: bpc.animTime });
+
+            $('.main-nav').addClass('mobile-open');
+        };
+
+        bpc.closeMobileNav = function() {
+            TweenMax.to('#line1', bpc.animTime, { rotation: 0, ease: Linear.easeNone, delay: 0 });
+            TweenMax.to('#line3', bpc.animTime, { rotation: 0, ease: Linear.easeNone, delay: 0 });
+
+            TweenMax.to('#line2', 0, { alpha: 1, ease: Quart.easeOut, delay: bpc.animTime });
+            TweenMax.to('#line1', bpc.animTime, { y: -8, ease: Quart.easeOut, delay: bpc.animTime });
+            TweenMax.to('#line3', bpc.animTime, { y: 8, ease: Quart.easeOut, delay: bpc.animTime });
+
+            $('.main-nav').removeClass('mobile-open');
+        };
+
         $('.main-header .hamburger').on(bpc.clickType, function(e) {
             e.preventDefault();
             if ($('.main-nav').hasClass('mobile-open')) {
@@ -234,89 +301,38 @@ jQuery(document).ready(function($) {
         $('.select-group').on(bpc.clickType, function() {
             if ($(this).find('.select').hasClass('open')) {
                 $(this).find('.select').removeClass('open');
-                $(this).find('ul').slideUp(250);
+                $(this).find('ul').slideUp(150);
             } else {
                 $(this).find('.select').addClass('open');
-                $(this).find('ul').slideDown(250);
+                $(this).find('ul').slideDown(150);
             }
         });
-        /*$('.select-group .current').on(bpc.clickType, function() {
-            if ($(this).parent().hasClass('open')) {
-                $(this).parent().removeClass('open');
-                $(this).parent().find('ul').slideUp(250);
-            } else {
-                $(this).parent().addClass('open');
-                $(this).parent().find('ul').slideDown(250);
-            }
-        });*/
+
         $('.select-group .select').on(bpc.clickType, 'li', function() {
             if (!$(this).hasClass('selected')) {
                 $('.select-group .select li.selected').removeClass('selected');
                 $(this).addClass('selected');
                 $('.select-group .select .current').text($(this).text());
 
-                bpc.version = $(this).attr('data-val');
-                window.history.pushState(bpc.version, null, '?v=' + bpc.version + location.hash);
+                bpc.pixiVersionString = $(this).attr('data-val');
+                bpc.majorPixiVersion = getMajorPixiVersion(bpc.pixiVersionString);
+                window.history.pushState(bpc.pixiVersionString, null, '?v=' + bpc.pixiVersionString + location.hash);
 
-                //$('#preview').attr('src', 'iframe.html?src='+bpc.currentSource+'&v='+bpc.version+'&plugins='+bpc.plugins);
                 bpc.generateIFrameContent();
-                $('.main-content').animate({ scrollTop: 0 }, 300);
+                $('.main-content').animate({ scrollTop: 0 }, 200);
             }
         });
 
         // Download
         $('.footer .download').on(bpc.clickType, function() {
-            bpc.SaveToDisk(bpc.currentSource, bpc.currentFilename);
+            bpc.SaveToDisk(bpc.exampleUrl, bpc.exampleFilename);
         });
 
         // Refresh Button
         $('.reload').on(bpc.clickType, function() {
-            bpc.jsSource = bpc.editor.getValue();
+            bpc.exampleSourceCode = bpc.editor.getValue();
             bpc.generateIFrameContent();
         });
-    };
-
-    bpc.generateIFrameContent = function() {
-        // Remove all iFrames and content
-        var iframes = document.querySelectorAll('iframe');
-        for (var i = 0; i < iframes.length; i++) {
-            iframes[i].parentNode.removeChild(iframes[i]);
-        }
-        $('#example').html('<iframe id="preview" src="blank.html"></iframe>');
-        // Generate HTML and insert into iFrame
-        var js = bpc.jsSource;
-        var pixiUrl = '';
-
-        var isLocal = bpc.version.substr(0, 5) === 'local';
-        if (isLocal) {
-            pixiUrl = "dist/pixi.js"
-        } else if (bpc.version.substr(0, 2) === 'v3') { // pull v3 from github cdn
-            pixiUrl = 'https://cdn.rawgit.com/GoodBoyDigital/pixi.js/' + bpc.version + '/bin/pixi.js';
-        } else { // other versions come from S3
-            pixiUrl = 'https://d157l7jdn8e5sf.cloudfront.net/' + bpc.version + '/pixi.js';
-        }
-
-        var html = '<!DOCTYPE html><html><head><style>body,html{margin:0px;height:100%;overflow:hidden;}canvas{width:100%;height:100%;}</style></head><body>';
-        html += '<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>';
-        html += '<script src="' + pixiUrl + '"></script>';
-
-        var plugins = bpc.plugins === '' ? [] : bpc.plugins.split(',');
-
-        if (bpc.version !== "release" && bpc.version !== "dev" && bpc.version.substring(0, 4) !== "next") {
-            html += '<script src="pixi-plugins/pixi-legacy.js"></script>';
-        }
-        for (i = 0; i < plugins.length; i++) {
-            html += '<script src="pixi-plugins/' + plugins[i] + '.js"></script>';
-        }
-
-        html += '<script>window.onload = function(){' + js + '}</script></body></html>';
-
-        var iframe = document.getElementById('preview');
-        var frameDoc = iframe.contentDocument || iframe.contentWindow.document;
-
-        frameDoc.open();
-        frameDoc.write(html);
-        frameDoc.close();
     };
 
     bpc.SaveToDisk = function(fileURL, fileName) {

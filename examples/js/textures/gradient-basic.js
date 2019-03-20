e6e8b36ee4f6e84ddd5cc22c862f2254b1a6a0d0
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-03-20 15:28:39 +0300

    gradient basic demo

---
+++ examples/js/textures/gradient-basic.js
@@ -0,0 +1,31 @@
+var app = new PIXI.Application(800, 600, { antialias: true });
+document.body.appendChild(app.view);
+
+function createGradTexture() {
+    // adjust it if somehow you need better quality for very very big images
+    var quality = 256;
+    var canvas = document.createElement('canvas');
+    canvas.width = quality;
+    canvas.height = 1;
+
+    var ctx = canvas.getContext('2d');
+
+    // use canvas2d API to create gradient
+    var grd = ctx.createLinearGradient(0, 0, quality, 0);
+    grd.addColorStop(0, 'red');
+    grd.addColorStop(1, 'white');
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, quality, 1);
+
+    return PIXI.Texture.fromCanvas(canvas);
+}
+
+var gradTexture = createGradTexture();
+
+var sprite = new PIXI.Sprite(gradTexture);
+sprite.position.set(100, 100);
+sprite.rotation = Math.PI / 8;
+sprite.width = 300;
+sprite.height = 50;
+app.stage.addChild(sprite);

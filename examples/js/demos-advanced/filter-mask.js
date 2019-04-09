b24a17976a5cbfac0a3e854c674b867a9ac5196a
rtalon <ruben.talon.argente@gmail.com>
2019-04-09 14:53:41 +0200

    Filter mask example

---
+++ examples/js/demos-advanced/filter-mask.js
@@ -0,0 +1,50 @@
+// for this example you have to move mouse
+
+var app = new PIXI.Application(800, 600);
+document.body.appendChild(app.view);
+var stage = app.stage;
+
+//prepare circle texture, that will be our brush
+var brush = new PIXI.Graphics();
+brush.beginFill(0xffffff);
+brush.drawCircle(0, 0, 50);
+brush.endFill();
+
+PIXI.loader.add('t1', 'examples/assets/bg_grass.jpg');
+PIXI.loader.load(setup);
+
+function setup(loader, resources) {
+    let background = new PIXI.Sprite(resources['t1'].texture);
+    stage.addChild(background);
+    background.width = app.screen.width;
+    background.height = app.screen.height;
+
+    // Dimension
+    let radius = 150;
+    let offset = 10; // depends on blur factor.
+
+    let circle = new PIXI.Graphics();
+    circle.beginFill(0xFF0000);
+    circle.drawCircle(radius + offset, radius + offset, radius);
+    circle.endFill();
+
+    // Filters
+    circle.filters = [new PIXI.filters.BlurFilter()];
+
+    let rect = new PIXI.Rectangle(0, 0, (radius + offset) * 2, (radius + offset) * 2);
+    let texture = app.renderer.generateTexture(circle, PIXI.SCALE_MODES.NEAREST, 1, rect);
+
+    focus = new PIXI.Sprite(texture);
+
+    stage.addChild(focus);
+    background.mask = focus;
+
+    app.stage.interactive = true;
+    app.stage.on('mousemove', pointerMove);
+
+    function pointerMove(event)
+    {
+        focus.position.x = event.data.global.x - focus.width * 0.5;
+        focus.position.y = event.data.global.y - focus.height * 0.5;
+    }
+}

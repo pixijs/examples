32bec46e7c102463caf0774f235f3c1365cace5a
rtalon <ruben.talon.argente@gmail.com>
2019-12-16 13:16:18 +0100

    Reorder folders Remove resolution param

--- examples/js/gsap3-interaction/gsap3-filters.js
+++ examples/js/gsap3-interaction/gsap3-filters.js
@@ -4,7 +4,6 @@ const app = new PIXI.Application({
     width: 800,
     height: 600,
     backgroundColor: 0x1099bb,
-    resolution: window.devicePixelRatio || 1,
 });
 
 document.body.appendChild(app.view);
--- examples/js/gsap3-interaction/gsap3-trail.js
+++ examples/js/gsap3-interaction/gsap3-trail.js
@@ -2,7 +2,6 @@ const app = new PIXI.Application({
     width: 800,
     height: 600,
     backgroundColor: 0x1099bb,
-    resolution: window.devicePixelRatio || 1,
 });
 
 document.body.appendChild(app.view);

92f280bf79304f0cf23e9051ef96bcd699a1fae8
Kieran Clare <kieran.clare@inseinc.com>
2020-02-17 12:09:29 +0000

    Added commenting and rearranged to look a little neater

--- required/examples/demos-advanced/screenshot.js
+++ required/examples/demos-advanced/screenshot.js
@@ -1,21 +1,27 @@
 var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
 
-var bunnyContainer = new PIXI.Container();
-var screenContainer = new PIXI.Container();
-
-bunnyContainer.pivot.set(0.5, 0.5);
-
-screenContainer.addChild( bunnyContainer );
-app.stage.addChild(screenContainer);
-
+// Bunny texture
 var texture = PIXI.Texture.fromImage('required/assets/basics/bunny.png');
 
-var screenShot = new PIXI.Sprite();
-screenShot.position.set( 500, 300 );
-app.stage.addChild(screenShot);
+// Text style object
+var style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#476bff'], // gradient
+    stroke: '#000dff',
+    strokeThickness: 5,
+    wordWrap: true,
+    wordWrapWidth: 440,
+});
 
-// Create a 5x5 grid of bunnies
+// Create containers
+//// Container for Bunnies
+var bunnyContainer = new PIXI.Container();
+bunnyContainer.pivot.set(0.5, 0.5);
+
+//// Create a 5x5 grid of bunnies
 for (var i = 0; i < 25; i++) {
     var bunny = new PIXI.Sprite(texture);
     bunny.anchor.set(0.5);
@@ -24,50 +30,46 @@ for (var i = 0; i < 25; i++) {
     bunnyContainer.addChild(bunny);
 }
 
-// Center on the screen
+//// Position bunnies in the right place
 bunnyContainer.x = 200;
 bunnyContainer.y = (app.screen.height - bunnyContainer.height) / 2;
 
-// Center bunny sprite in local container coordinates
+//// Center bunny sprite in local container coordinates
 bunnyContainer.pivot.x = bunnyContainer.width / 2;
 bunnyContainer.pivot.y = bunnyContainer.height / 2;
 
-// Add play text
-var style = new PIXI.TextStyle({
-    fontFamily: 'Arial',
-    fontSize: 36,
-    fontStyle: 'italic',
-    fontWeight: 'bold',
-    fill: ['#ffffff', '#00ff99'], // gradient
-    stroke: '#4a1850',
-    strokeThickness: 5,
-    dropShadow: true,
-    dropShadowColor: '#000000',
-    dropShadowBlur: 4,
-    dropShadowAngle: Math.PI / 6,
-    dropShadowDistance: 6,
-    wordWrap: true,
-    wordWrapWidth: 440,
-});
+//// Container that we'll take an image from
+var screenContainer = new PIXI.Container();
+screenContainer.addChild( bunnyContainer );
+app.stage.addChild(screenContainer);
 
-var playText = new PIXI.Text('Click To Take Screenshot', style);
-playText.x = Math.round((app.screen.width - playText.width) / 2);
-playText.y = Math.round(playText.height / 2);
-app.stage.addChild(playText);
+// Create the sprite that the screenshot will appear on
+var screenShot = new PIXI.Sprite();
+screenShot.position.set( 500, 300 );
+app.stage.addChild(screenShot);
 
+// Set up text object
+var screenshotText = new PIXI.Text('Click To Take Screenshot', style);
+screenshotText.x = Math.round((app.screen.width - screenshotText.width) / 2);
+screenshotText.y = Math.round(screenshotText.height / 2);
+app.stage.addChild(screenshotText);
+
+screenshotText.interactive = true;
+screenshotText.buttonMode = true;
+
+// Generates a texture object from a container, then give that texture to our sprite object
+// Note that we're not doing this with our 'bunnyContainer' because it'll just be a straight shot of the bunnies
+// and won't copy the rotations of the bunnies. Try putting in the bunnyContainer instead to see what I mean.
 var takeScreenshot = function () {
-    var screenTexture = app.renderer.generateTexture( screenContainer, PIXI.SCALE_MODES.NEAREST );
+    var screenTexture = app.renderer.generateTexture( screenContainer, PIXI.SCALE_MODES.LINEAR );
     screenShot.texture = screenTexture;
 };
 
-playText.interactive = true;
-playText.buttonMode = true;
-playText.addListener('pointerdown', takeScreenshot );
+// Wire that up to our text button
+screenshotText.addListener('pointerdown', takeScreenshot );
 
 // Listen for animate update
 app.ticker.add(function(delta) {
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
+    // Just so that screenshots look different every time, lets spin the bunnies around
     bunnyContainer.rotation += 0.05 * delta;
 });

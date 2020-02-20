491a7f441edcc4e527831dd683f6681052a84896
Kieran Clare <kieran.clare@inseinc.com>
2020-02-20 16:30:28 +0000

    Updated example with dampening to slow squares down, updated commenting

--- required/examples/advanced-demos/collision-detection.js
+++ required/examples/advanced-demos/collision-detection.js
@@ -1,3 +1,5 @@
+// Based somewhat on this article by Spicy Yoghurt
+// URL for further reading: https://spicyyoghurt.com/tutorials/html5-javascript-game-development/collision-detection-physics
 var app = new PIXI.Application( 800, 600, { backgroundColor: 0x1099bb } );
 document.body.appendChild( app.view );
 
@@ -21,38 +23,57 @@ function testForHit( object1, object2 ) {
     if ( !object1 || !object1.hitArea || !object2 || !object2.vertexData ) {
         return;
     }
-    var objectVertexList = object2.vertexData.reduce( function ( accumulator, vertex, index ) {
-        if ( index % 2 === 0 ) {
-            return accumulator.concat( new PIXI.Point( vertex ) );
-        } else {
-            accumulator[accumulator.length - 1].y = vertex;
-            return accumulator;
-        }
-    }, [] );
+    // Convert vertex data into a list of PIXI.Points
+    var objectVertexList = object2.vertexData.reduce(
+        function ( accumulator, vertex, index ) {
+            if ( index % 2 === 0 ) {
+                return accumulator.concat( new PIXI.Point( vertex ) );
+            } else {
+                accumulator[accumulator.length - 1].y = vertex;
+                return accumulator;
+            }
+        },
+    [] );
 
     return objectVertexList.some( function ( vertex ) {
         return object1.hitArea.contains( vertex.x, vertex.y );
     } );
 }
 
-// Calculates the results of a collision, allowing us to give an impulse that shoves
-// objects apart
-function collisionResponse( object1, object2, object1Acceleration, object2Acceleration, object1Mass, object2Mass ) {
+// Calculates the results of a collision, allowing us to give an impulse that
+// shoves objects apart
+function collisionResponse( object1Data, object2Data ) {
+    var { object1, object1Acceleration, object1Mass } = object1Data;
+    var { object2, object2Acceleration, object2Mass } = object2Data;
+
     if ( !object1 || !object2 ) {
         return;
     }
 
-    var vCollision = new PIXI.Point( object2.x - object1.x, object2.y - object1.y );
+    var vCollision = new PIXI.Point(
+        object2.x - object1.x,
+        object2.y - object1.y
+    );
     var distance = Math.sqrt(
         ( object2.x - object1.x ) * ( object2.x - object1.x ) +
         ( object2.y - object1.y ) * ( object2.y - object1.y )
     );
-    var vCollisionNorm = new PIXI.Point( vCollision.x / distance, vCollision.y / distance );
-    var vRelativeVelocity = new PIXI.Point( object1Acceleration.x - object2Acceleration.x, object1Acceleration.y - object2Acceleration.y );
-    var speed = vRelativeVelocity.x * vCollisionNorm.x + vRelativeVelocity.y * vCollisionNorm.y;
+    var vCollisionNorm = new PIXI.Point(
+        vCollision.x / distance,
+        vCollision.y / distance
+    );
+    var vRelativeVelocity = new PIXI.Point(
+        object1Acceleration.x - object2Acceleration.x,
+        object1Acceleration.y - object2Acceleration.y
+    );
+    var speed = vRelativeVelocity.x * vCollisionNorm.x +
+        vRelativeVelocity.y * vCollisionNorm.y;
     var impulse = impulsePower * speed / ( object1Mass + object2Mass );
 
-    return new PIXI.Point( impulse * vCollisionNorm.x, impulse * vCollisionNorm.y );
+    return new PIXI.Point(
+        impulse * vCollisionNorm.x,
+        impulse * vCollisionNorm.y
+    );
 }
 
 // Calculate the distance between two given points
@@ -74,7 +95,8 @@ var greenSquare = new PIXI.Graphics()
     )
     .endFill();
 
-// Create a hitarea that matches the shape, which will be used for point intersection
+// Create a hitarea that matches the shape, which will be used for point
+// intersection
 greenSquare.hitArea = new PIXI.Rectangle(
     ( app.screen.width - 100 ) / 2,
     ( app.screen.height - 100 ) / 2,
@@ -105,7 +127,10 @@ var redAcceleration = new PIXI.Point( 0 );
 
 // Listen for animate update
 app.ticker.add( function ( delta ) {
-    redAcceleration.set( 0 );
+    // Applied deacceleration for both squares, done by reducing the
+    // acceleration by 0.01% of the acceleration every loop
+    redAcceleration.set( redAcceleration.x * 0.99, redAcceleration.y * 0.99 );
+    greenAcceleration.set( greenAcceleration.x * 0.99, greenAcceleration.y * 0.99 );
 
     var mouseCoords = app.renderer.plugins.interaction.mouse.global;
 
@@ -127,35 +152,67 @@ app.ticker.add( function ( delta ) {
     colliding = testForHit( greenSquare, redSquare );
 
     // If the mouse is off screen, then don't update any further
-    if ( app.screen.width > mouseCoords.x || mouseCoords.x > 0 || app.screen.height > mouseCoords.y || mouseCoords.y > 0 ) {
+    if ( app.screen.width > mouseCoords.x || mouseCoords.x > 0 ||
+        app.screen.height > mouseCoords.y || mouseCoords.y > 0 ) {
         // Get the red square's center point
-        var redSquareCenterPosition = new PIXI.Point( redSquare.x + ( redSquare.width * 1.5 ), redSquare.y + ( redSquare.height * 1.5 ) );
+        var redSquareCenterPosition = new PIXI.Point(
+            redSquare.x + ( redSquare.width * 1.5 ),
+            redSquare.y + ( redSquare.height * 1.5 )
+        );
 
-        // Calculate the direction vector between the mouse pointer and the red square
-        var toMouseDirection = new PIXI.Point( mouseCoords.x - redSquareCenterPosition.x, mouseCoords.y - redSquareCenterPosition.y );
+        // Calculate the direction vector between the mouse pointer and
+        // the red square
+        var toMouseDirection = new PIXI.Point(
+            mouseCoords.x - redSquareCenterPosition.x,
+            mouseCoords.y - redSquareCenterPosition.y
+        );
 
         // Use the above to figure out the angle that direction has
-        var angleToMouse = Math.atan2( toMouseDirection.y, toMouseDirection.x );
+        var angleToMouse = Math.atan2(
+            toMouseDirection.y,
+            toMouseDirection.x
+        );
 
-        // Figure out the speed the square should be travelling by, as a function of how far away from the mouse pointer the red square is
-        var redSpeed = distanceBetweenTwoPoints( mouseCoords, redSquareCenterPosition ) * movementSpeed;
+        // Figure out the speed the square should be travelling by, as a
+        // function of how far away from the mouse pointer the red square is
+        var distMouseRedSquare = distanceBetweenTwoPoints(
+            mouseCoords,
+            redSquareCenterPosition
+        );
+        var redSpeed = distMouseRedSquare * movementSpeed;
 
         // Calculate the acceleration of the red square
-        redAcceleration.set( Math.cos( angleToMouse ) * redSpeed, Math.sin( angleToMouse ) * redSpeed );
+        redAcceleration.set(
+            Math.cos( angleToMouse ) * redSpeed,
+            Math.sin( angleToMouse ) * redSpeed
+        );
     }
 
     // If the two squares are colliding
     if ( colliding ) {
-        // Calculate the changes in acceleration that should be made between each square as a result
-        // of the collision
+        // Calculate the changes in acceleration that should be made between
+        // each square as a result of the collision
         var collisionPush = collisionResponse(
-            greenSquare, redSquare,
-            greenAcceleration, redAcceleration,
-            greenSquareMass, redSquareMass
+            {
+                object1: greenSquare,
+                object1Acceleration: greenAcceleration,
+                object1Mass: greenSquareMass
+            },
+            {
+                object2: redSquare,
+                object2Acceleration: redAcceleration,
+                object2Mass: redSquareMass
+            }
         );
         // Set the changes in acceleration for both squares
-        redAcceleration.set( ( collisionPush.x * greenSquareMass ), ( collisionPush.y * greenSquareMass ) );
-        greenAcceleration.set( -( collisionPush.x * redSquareMass ), -( collisionPush.y * redSquareMass ) );
+        redAcceleration.set(
+            ( collisionPush.x * greenSquareMass ),
+            ( collisionPush.y * greenSquareMass )
+        );
+        greenAcceleration.set(
+            -( collisionPush.x * redSquareMass ),
+            -( collisionPush.y * redSquareMass )
+        );
     }
 
     greenSquare.x += greenAcceleration.x * delta;
@@ -163,7 +220,8 @@ app.ticker.add( function ( delta ) {
     greenSquare.hitArea.x += greenAcceleration.x * delta;
     greenSquare.hitArea.y += greenAcceleration.y * delta;
 
-    // Apply the acceleration to the XY coords of the squares to update their positions
+    // Apply the acceleration to the XY coords of the squares to update their
+    // positions
     redSquare.x += redAcceleration.x * delta;
     redSquare.y += redAcceleration.y * delta;
 

80c49c64c658571e7bcbd638a2a96d6c128923a5
Kieran Clare <kieran.clare@inseinc.com>
2020-02-21 12:13:38 +0000

    Stripped down example, added new star sprite asset, updated commenting, lint sweep

--- examples/js/interaction/custom-hitarea.js
+++ examples/js/interaction/custom-hitarea.js
@@ -1,11 +1,84 @@
 const app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
 document.body.appendChild(app.view);
 
-// A few options to control how the bunny sprite moves
-let speedStep = 0.1;
-let spinSpeed = 0.1;
-const moveSpeed = 100;
-let moveBunny = false;
+const yellowStar = PIXI.Texture.from('examples/assets/yellowstar.png');
+
+// Standard Sprite Button
+const starButtonNoHitArea = new PIXI.Sprite(yellowStar);
+
+starButtonNoHitArea.position.set(125, 200);
+
+starButtonNoHitArea.buttonMode = true;
+starButtonNoHitArea.interactive = true;
+
+starButtonNoHitArea.on('pointerdown',
+    (event) => {
+        console.log('CLICK');
+        starButtonNoHitArea.tint = 0x333333;
+    });
+
+starButtonNoHitArea.on('pointerover',
+    (event) => {
+        console.log('TINT');
+        starButtonNoHitArea.tint = 0x666666;
+    });
+
+starButtonNoHitArea.on('pointerout',
+    (event) => {
+        starButtonNoHitArea.tint = 0xFFFFFF;
+    });
+
+// Custom Hitarea Button
+const starButtonWithHitArea = new PIXI.Sprite(yellowStar);
+starButtonWithHitArea.position.set(325, 200);
+
+// Create a hitarea that matches the sprite, which will be used for point
+// intersection
+starButtonWithHitArea.hitArea = new PIXI.Polygon([
+    404, 199,
+    424, 249,
+    484, 254,
+    439, 294,
+    454, 349,
+    404, 319,
+    354, 349,
+    369, 294,
+    324, 254,
+    384, 249,
+]);
+starButtonWithHitArea.buttonMode = true;
+starButtonWithHitArea.interactive = true;
+
+// Hitareas ignore masks. You can still click on a button made in this way,
+// even from areas covered by a mask
+const starButtonWithMask = new PIXI.Sprite(yellowStar);
+starButtonWithMask.position.set(525, 200);
+
+const squareMask = new PIXI.Graphics()
+    .beginFill(0xFFFFFF)
+    .drawRect(starButtonWithMask.x, starButtonWithMask.y, 75, 200)
+    .endFill();
+
+starButtonWithMask.mask = squareMask;
+
+// Again, hitarea for intersection checks
+starButtonWithMask.hitArea = new PIXI.Polygon([
+    604, 199,
+    624, 249,
+    684, 254,
+    669, 294,
+    654, 349,
+    604, 319,
+    554, 349,
+    569, 294,
+    524, 254,
+    584, 249,
+]);
+starButtonWithMask.buttonMode = true;
+starButtonWithMask.interactive = true;
+
+// Add to stage
+app.stage.addChild(starButtonWithHitArea, starButtonNoHitArea, starButtonWithMask, squareMask);
 
 // Test For Hit
 // Lets us check whether the mouse pointer intersects with the hitarea
@@ -17,95 +90,18 @@ function testForHit(button, event) {
     return button.hitArea.contains(mouseCoords.x, mouseCoords.y);
 }
 
-// For the first example, we'll make our button. You can use any shape instead of a
-// polygon
-const starButton = new PIXI.Graphics()
-    .beginFill(0xf1c40f)
-    .drawPolygon([
-        550, 20,
-        570, 70,
-        630, 75,
-        585, 115,
-        600, 170,
-        550, 140,
-        500, 170,
-        515, 115,
-        470, 75,
-        530, 70,
-    ])
-    .endFill();
-
-// Turns on interactive elements to show that these are buttons
-// to be clicked
-starButton.interactive = true;
-starButton.buttonMode = true;
-
-// Add to stage
-app.stage.addChild(starButton);
-
-// Create a hitarea that matches, which will be used for point intersection
-starButton.hitArea = new PIXI.Polygon([
-    550, 20,
-    570, 70,
-    630, 75,
-    585, 115,
-    600, 170,
-    550, 140,
-    500, 170,
-    515, 115,
-    470, 75,
-    530, 70,
-]);
-
-// This time, the same thing but for a sprite
-const bunnyButtonSprite = PIXI.Sprite.from('examples/assets/bunny.png');
-bunnyButtonSprite.x = 650;
-bunnyButtonSprite.y = 20;
-bunnyButtonSprite.scale.set(3);
-app.stage.addChild(bunnyButtonSprite);
-
-// Turns on interactive elements to show that these are buttons
-// to be clicked
-bunnyButtonSprite.interactive = true;
-bunnyButtonSprite.buttonMode = true;
-app.stage.addChild(bunnyButtonSprite);
-
-// We also add a polygon to represent the clickable area
-bunnyButtonSprite.hitArea = new PIXI.Polygon([
-    668, 24,
-    681, 24,
-    682, 37,
-    685, 37,
-    685, 41,
-    692, 41,
-    692, 37,
-    696, 37,
-    695, 24,
-    710, 24,
-    710, 127,
-    704, 127,
-    701, 113,
-    676, 114,
-    673, 127,
-    668, 127,
-    668, 24,
-]);
-
-// Register a function to happen on an event fired by
-// the InteractionManager. In short, instead of looking to hook
-// into the event of a displayObject being clicked, we instead
-// check if clicks are happening at all, then check whether it
-// hit our hitarea
+// Instead of using events from the sprite object, we register a function to
+// happen on an event fired by the InteractionManager. In short, instead of
+// looking to hook into the event of a displayObject being clicked, we instead
+// check if clicks are happening at all, then check whether it hit our hitarea.
 app.renderer.plugins.interaction.on('pointerdown',
     (event) => {
         console.log('CLICK');
-        if (testForHit(starButton, event)) {
-            spinSpeed = -spinSpeed;
-            speedStep = -speedStep;
+        if (testForHit(starButtonWithHitArea, event)) {
+            starButtonWithHitArea.tint = 0x333333;
         }
-
-        if (testForHit(bunnyButtonSprite, event)) {
-            moveBunny = !moveBunny;
+        if (testForHit(starButtonWithMask, event)) {
+            starButtonWithMask.tint = 0x333333;
         }
     });
 
@@ -113,46 +109,17 @@ app.renderer.plugins.interaction.on('pointerdown',
 // tinted when the mouse pointer intersects with the hitarea
 app.renderer.plugins.interaction.on('pointermove',
     (event) => {
-        if (testForHit(starButton, event)) {
+        if (testForHit(starButtonWithHitArea, event)) {
             console.log('TINT');
-            starButton.tint = 0x666666;
+            starButtonWithHitArea.tint = 0x666666;
         } else {
-            starButton.tint = 0xFFFFFF;
+            starButtonWithHitArea.tint = 0xFFFFFF;
         }
 
-        if (testForHit(bunnyButtonSprite, event)) {
+        if (testForHit(starButtonWithMask, event)) {
             console.log('TINT');
-            bunnyButtonSprite.tint = 0x666666;
+            starButtonWithMask.tint = 0x666666;
         } else {
-            bunnyButtonSprite.tint = 0xFFFFFF;
+            starButtonWithMask.tint = 0xFFFFFF;
         }
     });
-
-// Set Up Bunny Sprite
-const bunny = PIXI.Sprite.from('examples/assets/bunny.png');
-bunny.anchor.set(0.5);
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
-bunny.scale.set(2);
-app.stage.addChild(bunny);
-
-// Listen for animate update
-app.ticker.add((delta) => {
-    bunny.rotation += spinSpeed * delta;
-
-    // If the bunny button has been clicked, then move the bunny across
-    // the screen
-    if (moveBunny) {
-        bunny.x += (moveSpeed * spinSpeed) * delta;
-
-        // Check that the bunny doesn't fly off screen by making it zip back
-        // to the opposite side when it does
-        if (bunny.x < 0) {
-            bunny.x = app.screen.width;
-        }
-
-        if (bunny.x > app.screen.width) {
-            bunny.x = 0;
-        }
-    }
-});

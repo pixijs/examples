1bcf1ed4a8739f32fb557e7a2b0c01add6b37aa1
Kieran Clare <kieran.clare@inseinc.com>
2020-02-21 16:22:00 +0000

    Updated with an extra button to illustrate how the hit area works fully

--- examples/js/interaction/custom-hitarea.js
+++ examples/js/interaction/custom-hitarea.js
@@ -4,122 +4,154 @@ document.body.appendChild(app.view);
 const yellowStar = PIXI.Texture.from('examples/assets/yellowstar.png');
 
 // Standard Sprite Button
-const starButtonNoHitArea = new PIXI.Sprite(yellowStar);
+const starButton1 = new PIXI.Sprite(yellowStar);
 
-starButtonNoHitArea.position.set(125, 200);
+starButton1.position.set(50, 200);
 
-starButtonNoHitArea.buttonMode = true;
-starButtonNoHitArea.interactive = true;
+starButton1.buttonMode = true;
+starButton1.interactive = true;
 
-starButtonNoHitArea.on('pointerdown',
-    (event) => {
-        console.log('CLICK');
-        starButtonNoHitArea.tint = 0x333333;
-    });
+starButton1.pointerdown = (event) => {
+    starButton1.tint = 0x333333;
+};
 
-starButtonNoHitArea.on('pointerover',
-    (event) => {
-        console.log('TINT');
-        starButtonNoHitArea.tint = 0x666666;
-    });
+starButton1.pointerover = (event) => {
+    starButton1.tint = 0x666666;
+};
 
-starButtonNoHitArea.on('pointerout',
-    (event) => {
-        starButtonNoHitArea.tint = 0xFFFFFF;
-    });
+starButton1.pointerout = (event) => {
+    starButton1.tint = 0xFFFFFF;
+};
 
 // Custom Hitarea Button
-const starButtonWithHitArea = new PIXI.Sprite(yellowStar);
-starButtonWithHitArea.position.set(325, 200);
+const starButton2 = new PIXI.Sprite(yellowStar);
+starButton2.position.set(250, 200);
 
 // Create a hitarea that matches the sprite, which will be used for point
 // intersection
-starButtonWithHitArea.hitArea = new PIXI.Polygon([
-    404, 199,
-    424, 249,
-    484, 254,
-    439, 294,
-    454, 349,
-    404, 319,
-    354, 349,
-    369, 294,
-    324, 254,
-    384, 249,
+starButton2.hitArea = new PIXI.Polygon([
+    80, 0,
+    100, 50,
+    160, 55,
+    115, 95,
+    130, 150,
+    80, 120,
+    30, 150,
+    45, 95,
+    0, 55,
+    60, 50,
 ]);
-starButtonWithHitArea.buttonMode = true;
-starButtonWithHitArea.interactive = true;
+starButton2.buttonMode = true;
+starButton2.interactive = true;
 
-// Hitareas ignore masks. You can still click on a button made in this way,
-// even from areas covered by a mask
-const starButtonWithMask = new PIXI.Sprite(yellowStar);
-starButtonWithMask.position.set(525, 200);
+starButton2.pointerdown = (event) => {
+    starButton2.tint = 0x333333;
+};
+
+starButton2.pointerover = (event) => {
+    starButton2.tint = 0x666666;
+};
+
+starButton2.pointerout = (event) => {
+    starButton2.tint = 0xFFFFFF;
+};
+
+// With Mask, No Hit Area
+const starButton3 = new PIXI.Sprite(yellowStar);
+
+starButton3.position.set(450, 200);
+
+starButton3.buttonMode = true;
+starButton3.interactive = true;
 
 const squareMask = new PIXI.Graphics()
     .beginFill(0xFFFFFF)
-    .drawRect(starButtonWithMask.x, starButtonWithMask.y, 75, 200)
+    .drawRect(starButton3.x, starButton3.y, 75, 200)
     .endFill();
 
-starButtonWithMask.mask = squareMask;
+starButton3.mask = squareMask;
+
+starButton3.pointerdown = (event) => {
+    starButton3.tint = 0x333333;
+};
+
+starButton3.pointerover = (event) => {
+    starButton3.tint = 0x666666;
+};
+
+starButton3.pointerout = (event) => {
+    starButton3.tint = 0xFFFFFF;
+};
+
+// With a Mask and Hit Area
+// Hitareas ignore masks. You can still click on a button made in this way,
+// even from areas covered by a mask
+const starButton4 = new PIXI.Sprite(yellowStar);
+starButton4.position.set(600, 200);
+
+const squareMask2 = new PIXI.Graphics()
+    .beginFill(0xFFFFFF)
+    .drawRect(starButton4.x, starButton4.y, 75, 200)
+    .endFill();
+
+starButton4.mask = squareMask2;
 
 // Again, hitarea for intersection checks
-starButtonWithMask.hitArea = new PIXI.Polygon([
-    604, 199,
-    624, 249,
-    684, 254,
-    669, 294,
-    654, 349,
-    604, 319,
-    554, 349,
-    569, 294,
-    524, 254,
-    584, 249,
+starButton4.hitArea = new PIXI.Polygon([
+    80, 0,
+    100, 50,
+    160, 55,
+    115, 95,
+    130, 150,
+    80, 120,
+    30, 150,
+    45, 95,
+    0, 55,
+    60, 50,
 ]);
-starButtonWithMask.buttonMode = true;
-starButtonWithMask.interactive = true;
+starButton4.buttonMode = true;
+starButton4.interactive = true;
+
+starButton4.pointerdown = (event) => {
+    starButton4.tint = 0x333333;
+};
+
+starButton4.pointerover = (event) => {
+    starButton4.tint = 0x666666;
+};
+
+starButton4.pointerout = (event) => {
+    starButton4.tint = 0xFFFFFF;
+};
+
+const style = new PIXI.TextStyle({ fill: '#ffffff' });
+
+const text1 = new PIXI.Text('Standard', style);
+text1.x = starButton1.x + 25;
+text1.y = starButton1.y + 170;
+
+const text2 = new PIXI.Text('Hit Area', style);
+text2.x = starButton2.x + 35;
+text2.y = starButton2.y + 170;
+
+const text3 = new PIXI.Text('Mask', style);
+text3.x = starButton3.x + 10;
+text3.y = starButton3.y + 170;
+
+const text4 = new PIXI.Text('Mask + Hit Area', style);
+text4.x = starButton4.x - 10;
+text4.y = starButton4.y + 170;
 
 // Add to stage
-app.stage.addChild(starButtonWithHitArea, starButtonNoHitArea, starButtonWithMask, squareMask);
-
-// Test For Hit
-// Lets us check whether the mouse pointer intersects with the hitarea
-function testForHit(button, event) {
-    if (button === null || button.hitArea === null) {
-        return false;
-    }
-    const mouseCoords = event.data.global;
-    return button.hitArea.contains(mouseCoords.x, mouseCoords.y);
-}
-
-// Instead of using events from the sprite object, we register a function to
-// happen on an event fired by the InteractionManager. In short, instead of
-// looking to hook into the event of a displayObject being clicked, we instead
-// check if clicks are happening at all, then check whether it hit our hitarea.
-app.renderer.plugins.interaction.on('pointerdown',
-    (event) => {
-        console.log('CLICK');
-        if (testForHit(starButtonWithHitArea, event)) {
-            starButtonWithHitArea.tint = 0x333333;
-        }
-        if (testForHit(starButtonWithMask, event)) {
-            starButtonWithMask.tint = 0x333333;
-        }
-    });
-
-// We can do something similar, but this time the object will be
-// tinted when the mouse pointer intersects with the hitarea
-app.renderer.plugins.interaction.on('pointermove',
-    (event) => {
-        if (testForHit(starButtonWithHitArea, event)) {
-            console.log('TINT');
-            starButtonWithHitArea.tint = 0x666666;
-        } else {
-            starButtonWithHitArea.tint = 0xFFFFFF;
-        }
-
-        if (testForHit(starButtonWithMask, event)) {
-            console.log('TINT');
-            starButtonWithMask.tint = 0x666666;
-        } else {
-            starButtonWithMask.tint = 0xFFFFFF;
-        }
-    });
+app.stage.addChild(
+    starButton2,
+    starButton1,
+    starButton3,
+    starButton4,
+    squareMask,
+    squareMask2,
+    text1,
+    text2,
+    text3,
+    text4,
+);

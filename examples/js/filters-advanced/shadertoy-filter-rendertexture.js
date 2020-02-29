2b8c8b81ed293f9d53f13932ad193210dc962807
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2020-02-29 19:37:07 +0200

    Fixed shadertoy filter exception and made it use uSampler so using filter makes more sense.

--- examples/js/filters-advanced/shadertoy-filter-rendertexture.js
+++ examples/js/filters-advanced/shadertoy-filter-rendertexture.js
@@ -1,7 +1,6 @@
 /**
-Please note that this is not the most optimal way of doing pure shader generated rendering.
-Check the mesh version of example for more performant version.
-This is on the other hand useful if something is being done that does not update every frame, just replace the filter scene with actual content.
+Please note that this is not the most optimal way of doing pure shader generated rendering and should be used when scene is wanted as input texture.
+Check the mesh version of example for more performant version if you need only shader generated content.
 **/
 
 const app = new PIXI.Application({ backgroundColor: 0x1099bb });
@@ -12,13 +11,12 @@ app.loader
     .load(onAssetsLoaded);
 
 let filter = null;
-const renderTexture = PIXI.RenderTexture.create(200, 200);
-const filterContainer = new PIXI.Container();
 
-filterContainer.filterArea = new PIXI.Rectangle(0,0, 200, 200);
-const sprite = new PIXI.Sprite( new PIXI.Texture(renderTexture));
-sprite.anchor.set(0.5, 0.0);
-app.stage.addChild(sprite);
+let text = new PIXI.Text("PixiJS", {fill:0xFFFFFF, fontSize:80});
+text.anchor.set(0.5, 0.5);
+text.position.set(app.renderer.screen.width/2, app.renderer.screen.height/2);
+
+app.stage.addChild(text);
 
 let totalTime = 0;
 
@@ -26,6 +24,7 @@ let totalTime = 0;
 const fragment = `//Based on this: https://www.shadertoy.com/view/wtlSWX
 
 varying vec2 vTextureCoord;
+uniform sampler2D uSampler;
 uniform sampler2D noise;
 uniform float time;
 //Distance function. Just calculates the height (z) from x,y plane with really simple length check. Its not exact as there could be shorter distances.
@@ -34,7 +33,8 @@ vec2 dist(vec3 p)
   float id = floor(p.x)+floor(p.y);
   id = mod(id, 2.);
   float h = texture2D(noise, vec2(p.x, p.y)*0.04).r*5.1;
-  return vec2(h-p.z,id);
+  float h2 = texture2D(uSampler, vTextureCoord).r;
+  return vec2(h+h2-p.z,id);
 }
 //Light calculation.
 vec3 calclight(vec3 p, vec3 rd)
@@ -94,22 +94,20 @@ function onAssetsLoaded(loader) {
 	loader.resources["examples/assets/perlin.jpg"].texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
 	loader.resources["examples/assets/perlin.jpg"].texture.baseTexture.mipmap = false;
 	
-	const perlin = new PIXI.Sprite(loader.resources["examples/assets/perlin.jpg"].texture);
+	const perlin = loader.resources["examples/assets/perlin.jpg"].texture;
 	perlin.width = perlin.height = 200;
-	filterContainer.addChild(perlin);
+	
 	//Build the filter
 	filter = new PIXI.Filter(null, fragment, {
 		time:0.0,
 		noise:perlin
-	});
-	filterContainer.filters = [filter];
+  });
+  app.stage.filterArea = app.renderer.screen;
+	app.stage.filters = [filter];
 	
     // Listen for animate update.
     app.ticker.add((delta) => {
-        if(true) //Check if rendering for RT actually needs to happen, for now always
-		{
-			app.renderer.render(filterContainer, renderTexture);
-		}
+
 		filter.uniforms.time = totalTime;
 		totalTime += delta/60;
 		
@@ -118,11 +116,6 @@ function onAssetsLoaded(loader) {
 		let npy = Math.sin(totalTime*0.45)*100 + 200;
 		
 		let dx = (Math.cos((totalTime-10/60)*0.5)*250 + 400) - npx;
-		
-		sprite.x = npx;
-		sprite.y = npy;
-		
-		sprite.rotation = Math.atan2(sprite.y-8, dx)-Math.PI/2;
 		
     });
 }

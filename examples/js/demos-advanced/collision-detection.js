1340d7fae467f76b222329112934aa17d187a6d4
ben byford <benjaminbyford@gmail.com>
2020-08-20 11:27:22 +0100

    correction for collision
    I copied this into my own project and found it had strange behaviour, so i googled for a bit and found that it needed a slight correction.

--- examples/js/demos-advanced/collision-detection.js
+++ examples/js/demos-advanced/collision-detection.js
@@ -19,7 +19,7 @@ function testForAABB(object1, object2) {
     return bounds1.x < bounds2.x + bounds2.width
         && bounds1.x + bounds2.width > bounds2.x
         && bounds1.y < bounds2.y + bounds2.height
-        && bounds1.y + bounds2.height > bounds2.y;
+        && bounds1.y + bounds1.height > bounds2.y;
 }
 
 // Calculates the results of a collision, allowing us to give an impulse that

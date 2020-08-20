7432d3bfd31ba21c096be8bee767a325fb644c81
ben byford <benjaminbyford@gmail.com>
2020-08-20 12:26:40 +0100

    update to line 20
    yep double checked and your right

--- examples/js/demos-advanced/collision-detection.js
+++ examples/js/demos-advanced/collision-detection.js
@@ -17,7 +17,7 @@ function testForAABB(object1, object2) {
     const bounds2 = object2.getBounds();
 
     return bounds1.x < bounds2.x + bounds2.width
-        && bounds1.x + bounds2.width > bounds2.x
+        && bounds1.x + bounds1.width > bounds2.x
         && bounds1.y < bounds2.y + bounds2.height
         && bounds1.y + bounds1.height > bounds2.y;
 }

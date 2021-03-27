5caf79a4eb969217b5facb24bcb530811884ff15
Shukant Pal <sukantk2002@outlook.com>
2021-03-27 13:23:38 -0400

    Don't inherit Application, add slider example

--- examples/js/events/drag-n-drop.js
+++ examples/js/events/drag-n-drop.js
@@ -1,86 +1,108 @@
+// Disable interaction plugin (for PixiJS 6)
 // eslint-disable-next-line no-underscore-dangle
 delete PIXI.Renderer.__plugins.interaction;
 
-class DnDApplication extends PIXI.Application {
-    constructor() {
-        super({ backgroundColor: 0x1099bb });
-
-        // Make sure stage captures all events when interactive
-        this.stage.hitArea = this.renderer.screen;
-
-        // Install EventSystem, if not already (PixiJS 6 doesn't add it by default)
-        if (!('event' in this.renderer)) {
-            this.renderer.addSystem(PIXI.EventSystem, 'events');
-        }
-
-        // create a texture from an image path
-        this.imageTexture = PIXI.Texture.from('examples/assets/bunny.png');
-
-        // Scale mode for pixelation
-        this.imageTexture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-
-        for (let i = 0; i < 10; i++) {
-            this.stage.addChild(this.makeBunny(
-                Math.floor(Math.random() * this.screen.width),
-                Math.floor(Math.random() * this.screen.height),
-            ));
-        }
-    }
-
-    makeBunny(x, y) {
-        // create our little bunny friend..
-        const bunny = new PIXI.Sprite(this.imageTexture);
-
-        // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-        bunny.interactive = true;
-
-        // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-        bunny.buttonMode = true;
-
-        // center the bunny's anchor point
-        bunny.anchor.set(0.5);
-
-        // make it a bit bigger, so it's easier to grab
-        bunny.scale.set(3);
-
-        // setup events for mouse + touch using
-        // the pointer events
-        bunny.addEventListener('pointerdown', onDragStart);
-        bunny.addEventListener('pointerup', onDragEnd);
-        bunny.addEventListener('pointerupoutside', onDragEnd);
-
-        // move the sprite to its designated position
-        bunny.x = x;
-        bunny.y = y;
-
-        return bunny;
-    }
-}
-
-const app = new DnDApplication();
-let dragTarget = null;
-
+// Create app
+const app = new PIXI.Application({
+    antialias: true,
+    autoDensity: true,
+    backgroundColor: 0x1099bb,
+    resolution: devicePixelRatio,
+});
 document.body.appendChild(app.view);
 
-// Make bunny semi-transparent and listen to drag-move events when one is pressed.
-function onDragStart(e) {
-    e.target.alpha = 0.5;
-    dragTarget = e.target;
+// Install EventSystem, if not already (PixiJS 6 doesn't add it by default)
+if (!('event' in app.renderer)) {
+    app.renderer.addSystem(PIXI.EventSystem, 'events');
+}
 
-    app.stage.interactive = true;
+// Create a texture from an image path
+const imageTexture = PIXI.Texture.from('examples/assets/bunny.png');
+
+// Make the whole scene interactive
+app.stage.interactive = true;
+// Make sure stage captures all events when interactive
+app.stage.hitArea = app.renderer.screen;
+// Handle clicks on the canvas
+app.stage.addEventListener('click', onClick);
+
+// Scale mode for pixelation
+imageTexture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+
+// Populate scene graph with bunnies
+for (let i = 0; i < 10; i++) {
+    // Create our little bunny friend..
+    const bunny = new PIXI.Sprite(imageTexture);
+
+    // Enable the bunny to be interactive... this will allow it to respond to
+    // mouse and touch events
+    bunny.interactive = true;
+
+    // This button mode will mean the hand cursor appears when you roll over
+    // the bunny with your mouse
+    bunny.buttonMode = true;
+
+    // Center the bunny's anchor point.
+    bunny.anchor.set(0.5);
+
+    // Make it a bit bigger, so it's easier to grab.
+    bunny.scale.set(3);
+
+    // Setup events for mouse + touch using the pointer events
+    bunny.addEventListener('pointerdown', onDragStart);
+    bunny.addEventListener('pointerup', onDragEnd);
+    bunny.addEventListener('pointerupoutside', onDragEnd);
+
+    // Move the sprite to its designated position
+    bunny.x = Math.floor(Math.random() * app.screen.width);
+    bunny.y = Math.floor(Math.random() * app.screen.height);
+
+    // Add it into the scene
+    app.stage.addChild(bunny);
+}
+
+// Add a description
+const title = app.stage.addChild(new PIXI.Text(
+    'Drag a bunny around! Then click around the canvas to move the bunny to your pointer!',
+    {
+        fontSize: 12,
+    },
+));
+title.position.set(12, 12);
+
+// Store the bunny being dragged
+let selectedTarget;
+
+// Make bunny semi-transparent and listen to drag-move events when one is
+// pressed.
+function onDragStart(e) {
+    // Show that the bunny can now be dragged.
+    e.target.alpha = 0.5;
+    selectedTarget = e.target;
+
+    // Start listening to dragging on the stage
     app.stage.addEventListener('pointermove', onDragMove);
 }
 
-// Restore the dragTarget bunny's alpha & deregister listener when the bunny is release.
-function onDragEnd(e) {
-    dragTarget.alpha = 1;
-    dragTarget = null;
+// Restore the dragTarget bunny's alpha & deregister listener when the bunny is
+// released.
+function onDragEnd() {
+    // Restore the original bunny alpha.
+    selectedTarget.alpha = 1;
 
-    app.stage.interactive = false;
+    // Stop listening to dragging on the stage
     app.stage.removeEventListener('pointermove', onDragMove);
 }
 
 // Copy the position of the cursor into the dragTarget's position.
 function onDragMove(e) {
-    dragTarget.parent.toLocal(e.global, null, dragTarget.position);
+    // Don't use e.target because the pointer might move out of the bunny if
+    // the user drags fast, which would make e.target become the stage.
+    selectedTarget.parent.toLocal(e.global, null, selectedTarget.position);
+}
+
+function onClick(e) {
+    if (selectedTarget) {
+        selectedTarget.position.copyFrom(e.global);
+    }
 }

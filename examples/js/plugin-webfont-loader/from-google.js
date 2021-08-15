9514a81b8ee191d3c3a07bdc163c84f1ff8b518c
MiltonCandelero <miltoncandelero@gmail.com>
2021-08-14 22:27:05 -0300

    linting

--- examples/js/plugin-webfont-loader/from-google.js
+++ examples/js/plugin-webfont-loader/from-google.js
@@ -7,7 +7,6 @@ document.body.appendChild(app.view);
 app.loader.add({ name: 'From Google 1', url: 'https://fonts.googleapis.com/css2?family=Montserrat' });
 app.loader.add({ name: 'From Google 2', url: 'https://fonts.googleapis.com/css2?family=WindSong' });
 
-
 app.loader.onComplete.once(() => {
     const text1 = new PIXI.Text('This text uses the\nMonserrat font from Google', new PIXI.TextStyle({ fontFamily: 'Montserrat', fontSize: 55 }));
     const text2 = new PIXI.Text('This text uses the\nWindSong font from Google', new PIXI.TextStyle({ fontFamily: 'WindSong', fontSize: 55 }));

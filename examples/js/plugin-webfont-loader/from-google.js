e523f633d15a10be9af8e1002c9459d71bb532a9
Milton Candelero <miltoncandelero@gmail.com>
2021-08-15 20:58:55 -0300

    Added demos for pixi-webfont-loader (#142)

---
+++ examples/js/plugin-webfont-loader/from-google.js
@@ -0,0 +1,18 @@
+PIXI.Loader.registerPlugin(PIXI.WebfontLoaderPlugin);
+
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+// Load directly from google CSS!
+app.loader.add({ name: 'From Google 1', url: 'https://fonts.googleapis.com/css2?family=Montserrat' });
+app.loader.add({ name: 'From Google 2', url: 'https://fonts.googleapis.com/css2?family=WindSong' });
+
+app.loader.load(() => {
+    const text1 = new PIXI.Text('This text uses the\nMonserrat font from Google', new PIXI.TextStyle({ fontFamily: 'Montserrat', fontSize: 55 }));
+    const text2 = new PIXI.Text('This text uses the\nWindSong font from Google', new PIXI.TextStyle({ fontFamily: 'WindSong', fontSize: 55 }));
+
+    text2.y = 300;
+
+    app.stage.addChild(text1);
+    app.stage.addChild(text2);
+});

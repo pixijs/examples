bffcffa88878cee65a0840cfd38a8c49aac5d125
Matt Karl <matt@mattkarl.com>
2022-11-07 16:21:15 -0500

    Improve drawing example

--- public/examples/js/demos-advanced/scratchcard.js
+++ public/examples/js/demos-advanced/scratchcard.js
@@ -1,54 +1,70 @@
-// for this example you have to use mouse or touchscreen
-
 const app = new PIXI.Application();
 document.body.appendChild(app.view);
-const { stage } = app;
 
 // prepare circle texture, that will be our brush
-const brush = new PIXI.Graphics();
-brush.beginFill(0xffffff);
-brush.drawCircle(0, 0, 50);
-brush.endFill();
+const brush = new PIXI.Graphics()
+    .beginFill(0xffffff)
+    .drawCircle(0, 0, 50);
+
+// Create a line that will interpolate the drawn points
+const line = new PIXI.Graphics();
 
 PIXI.Assets.add('t1', 'examples/assets/bg_grass.jpg');
 PIXI.Assets.add('t2', 'examples/assets/bg_rotate.jpg');
 PIXI.Assets.load(['t1', 't2']).then(setup);
 
 function setup() {
-    const background = PIXI.Sprite.from('t1');
-    stage.addChild(background);
-    background.width = app.screen.width;
-    background.height = app.screen.height;
+    const { width, height } = app.screen;
+    const stageSize = { width, height };
 
-    const imageToReveal = PIXI.Sprite.from('t2');
-    stage.addChild(imageToReveal);
-    imageToReveal.width = app.screen.width;
-    imageToReveal.height = app.screen.height;
-
-    const renderTexture = PIXI.RenderTexture.create({
-        width: app.screen.width, height: app.screen.height,
-    });
-
+    const background = Object.assign(PIXI.Sprite.from('t1'), stageSize);
+    const imageToReveal = Object.assign(PIXI.Sprite.from('t2'), stageSize);
+    const renderTexture = PIXI.RenderTexture.create(stageSize);
     const renderTextureSprite = new PIXI.Sprite(renderTexture);
-    stage.addChild(renderTextureSprite);
+
     imageToReveal.mask = renderTextureSprite;
 
+    app.stage.addChild(
+        background,
+        imageToReveal,
+        renderTextureSprite,
+    );
+
     app.stage.interactive = true;
-    app.stage.on('pointerdown', pointerDown);
-    app.stage.on('pointerup', pointerUp);
-    app.stage.on('pointermove', pointerMove);
+    app.stage.hitArea = app.screen;
+    app.stage
+        .on('pointerdown', pointerDown)
+        .on('pointerup', pointerUp)
+        .on('pointerupoutside', pointerUp)
+        .on('pointermove', pointerMove);
 
     let dragging = false;
-
-    function pointerMove(event) {
+    let lastDrawnPoint = null;
+    function pointerMove({ global: { x, y } }) {
         if (dragging) {
-            brush.position.copyFrom(event.data.global);
+            brush.position.set(x, y);
             app.renderer.render(brush, {
                 renderTexture,
                 clear: false,
-                transform: null,
                 skipUpdateTransform: false,
             });
+            // Smooth out the drawing a little bit to make it look nicer
+            // this connects the previous drawn point to the current one
+            // using a line
+            if (lastDrawnPoint) {
+                line
+                    .clear()
+                    .lineStyle({ width: 100, color: 0xffffff })
+                    .moveTo(lastDrawnPoint.x, lastDrawnPoint.y)
+                    .lineTo(x, y);
+                app.renderer.render(line, {
+                    renderTexture,
+                    clear: false,
+                    skipUpdateTransform: false,
+                });
+            }
+            lastDrawnPoint = lastDrawnPoint || new PIXI.Point();
+            lastDrawnPoint.set(x, y);
         }
     }
 
@@ -59,5 +75,6 @@ function setup() {
 
     function pointerUp(event) {
         dragging = false;
+        lastDrawnPoint = null;
     }
 }

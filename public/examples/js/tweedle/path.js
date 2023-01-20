eb4a30dc1fce3ae7205b895c65fb410b52ebbe26
eCode <miltoncandelero@gmail.com>
2023-01-20 14:20:45 -0300

    feat(tweedle) added tween examples

---
+++ public/examples/js/tweedle/path.js
@@ -0,0 +1,44 @@
+// Interpolations Documentation: https://miltoncandelero.github.io/tweedle.js/variables/Interpolation.html
+
+const app = new PIXI.Application({ background: '#1099bb' });
+document.body.appendChild(app.view);
+
+// Update the shared group
+app.ticker.add(() => TWEEDLE.Group.shared.update());
+
+// Create a new texture
+const texture = PIXI.Texture.from('examples/assets/bunny.png');
+
+// time animation in miliseconds!
+const time = 10000;
+
+const xTargets = [100, 500, 100, 300, 700, 0];
+const yTargets = [200, 100, 500, 100, 500, 0];
+
+// Linear bunny
+const bunny1 = new PIXI.Sprite(texture);
+bunny1.tint = 0xFF0000;
+app.stage.addChild(bunny1);
+
+new TWEEDLE.Tween(bunny1).to({ x: xTargets, y: yTargets }, time)
+    .repeat(Infinity)
+    .start();
+
+// catmullRom bunny
+const bunny2 = new PIXI.Sprite(texture);
+bunny2.tint = 0x00FF00;
+app.stage.addChild(bunny2);
+
+new TWEEDLE.Tween(bunny2).to({ x: xTargets, y: yTargets }, time)
+    .repeat(Infinity)
+    .interpolation(TWEEDLE.Interpolation.Geom.CatmullRom)
+    .start();
+
+
+const auxGraphics = new PIXI.Graphics();
+auxGraphics.lineStyle(2, 0xFFFFFF, 1);
+for (let i = 0; i < xTargets.length; i++) {
+    auxGraphics.lineTo(xTargets[i], yTargets[i]);
+    auxGraphics.drawCircle(xTargets[i], yTargets[i], 5);
+}
+app.stage.addChild(auxGraphics);
---
+++ public/examples/js/tweedle/smart.js
@@ -0,0 +1,64 @@
+// Interpolations Documentation: https://miltoncandelero.github.io/tweedle.js/variables/Interpolation.html
+
+const app = new PIXI.Application({ background: '#1099bb' });
+document.body.appendChild(app.view);
+
+// Update the shared group
+app.ticker.add(() => TWEEDLE.Group.shared.update());
+
+// Create a new texture
+const texture = PIXI.Texture.from('examples/assets/bunny.png');
+
+// time animation in miliseconds!
+const time = 2000;
+
+// Angle and Radians supported!
+
+const directRotation = new PIXI.Sprite(texture);
+directRotation.anchor.set(0.5);
+directRotation.scale.set(3);
+directRotation.position.set(100, 100);
+app.stage.addChild(directRotation);
+
+const smartAngle = new PIXI.Sprite(texture);
+smartAngle.anchor.set(0.5);
+smartAngle.scale.set(3);
+smartAngle.position.set(300, 100);
+app.stage.addChild(smartAngle);
+
+const smartRotation = new PIXI.Sprite(texture);
+smartRotation.anchor.set(0.5);
+smartRotation.scale.set(3);
+smartRotation.position.set(500, 100);
+app.stage.addChild(smartRotation);
+
+// RGB
+
+const directTint = new PIXI.Sprite(texture);
+directTint.anchor.set(0.5);
+directTint.scale.set(3);
+directTint.position.set(100, 300);
+app.stage.addChild(directTint);
+
+const rgbTint = new PIXI.Sprite(texture);
+rgbTint.anchor.set(0.5);
+rgbTint.scale.set(3);
+rgbTint.position.set(300, 300);
+app.stage.addChild(rgbTint);
+
+
+app.stage.interactive = true;
+app.stage.on('pointerdown', () => {
+    const randomRadians = Math.random() * Math.PI * 2 * (Math.random() > 0.5 ? 1 : -1);
+    const randomDegrees = randomRadians * 180 / Math.PI;
+    const randomColor = Math.random() * 0xFFFFFF;
+
+    // Pay attention! Interpolation only works with arrays so we make an array of one element!
+
+    new TWEEDLE.Tween(directRotation).to({ rotation: randomRadians }, time).start();
+    new TWEEDLE.Tween(smartRotation).to({ rotation: [randomRadians] }, time).interpolation(TWEEDLE.Interpolation.Angle.Radians).start();
+    new TWEEDLE.Tween(smartAngle).to({ angle: [randomDegrees] }, time).interpolation(TWEEDLE.Interpolation.Angle.Degrees).start();
+
+    new TWEEDLE.Tween(directTint).to({ tint: randomColor }, time).start();
+    new TWEEDLE.Tween(rgbTint).to({ tint: [randomColor] }, time).interpolation(TWEEDLE.Interpolation.Color.RGB).start();
+});

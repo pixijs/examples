7773c4fd02da3ddf8855e5d9b0c0e433bc17b6e4
Matt Karl <matt@mattkarl.com>
2023-02-17 09:08:44 -0500

    Upgrade RenderTexture APIs

--- public/examples/js/textures/render-texture-basic.js
+++ public/examples/js/textures/render-texture-basic.js
@@ -14,7 +14,12 @@ for (let i = 0; i < 25; i++) {
     container.addChild(bunny);
 }
 
-const brt = new PIXI.BaseRenderTexture(300, 300, PIXI.SCALE_MODES.LINEAR, 1);
+const brt = new PIXI.BaseRenderTexture({
+  width: 300,
+  height: 300,
+  scaleMode: PIXI.SCALE_MODES.LINEAR,
+  resolution: 1,
+});
 const rt = new PIXI.RenderTexture(brt);
 
 const sprite = new PIXI.Sprite(rt);
@@ -33,5 +38,5 @@ container.x = 100;
 container.y = 60;
 
 app.ticker.add(() => {
-    app.renderer.render(container, rt);
+    app.renderer.render(container, { renderTexture: rt });
 });
